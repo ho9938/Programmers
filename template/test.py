0b8a57c2f79a54cf0a4236e7d7795567c408335d
Yoon Changho <ho9938@unist.ac.kr>
2023-02-13 23:24:25 +0900

    modify template: add readInput() function to take wide range of inputs

--- template/test.py
+++ template/test.py
@@ -1,3 +1,22 @@
 #!/usr/bin/python3
 
-print()
+def solution():
+    return 0
+    
+def readInput():
+    inputList = [elem.strip('"[] ') for elem in input().strip().split(',')]
+
+    # change type if int or float
+    if inputList[0].replace('-', '', 1).isdigit():
+        inputList = [int(elem) for elem in inputList]
+    elif inputList[0].replace('-', '', 1).replace('.', '', 1).isdigit():
+        inputList = [float(elem) for elem in inputList]
+
+    # change type if not list
+    if len(inputList) == 1:
+        return inputList[0]
+    else:
+        return inputList
+    
+if __name__ == '__main__':
+    print(solution())
